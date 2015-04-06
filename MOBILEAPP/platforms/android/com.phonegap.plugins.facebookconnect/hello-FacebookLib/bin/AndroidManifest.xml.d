c299ee002bd6562a8c44a77b7924d6f9ede24c42
satheesh <satheesh@3eplc.com>
2015-04-06 12:00:48 +0530

    Initial Code commit

--- MOBILEAPP/platforms/android/com.phonegap.plugins.facebookconnect/hello-FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/var/www/projects/wraskit/platforms/android/com.phonegap.plugins.facebookconnect/hello-FacebookLib/ant-build/AndroidManifest.xml : \
-/var/www/projects/wraskit/platforms/android/com.phonegap.plugins.facebookconnect/hello-FacebookLib/AndroidManifest.xml \
